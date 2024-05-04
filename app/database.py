807a4dbfb0f3344a30f12a1e63f3b6cfbbd7f0a9
Victor Adrianovskiy <VAdrianovskiy@gmail.com>
2024-05-04 15:10:49 +0300

    lesson 2

--- app/database.py
+++ app/database.py
@@ -1,14 +1,27 @@
+import asyncio
 from sqlalchemy import create_engine, text
+from sqlalchemy.ext.asyncio import create_async_engine
 from config import settings
 
-engine = create_engine(
+sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
-    echo=True,
+    echo=False,
     # pool_size=5,
     # max_overflow=10,
 )
 
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+)
 
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f"{res=}")
+# with sync_engine.connect() as conn:
+#     res = conn.execute(text("SELECT 1, 2, 3 union select 4, 5, 6"))
+#     print(f"{res.first()=}")
+
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1, 2, 3 union select 4, 5, 6"))
+        print(f"{res.first()=}")
+        
+asyncio.run(get_123())
